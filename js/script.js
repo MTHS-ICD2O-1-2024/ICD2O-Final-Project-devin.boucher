e9f84097afee3c41f86057e2ae2124bb4602fa45
Devin-boucher <devin.boucher@stu.ocsb.ca>
2025-05-28 13:01:46 -0400

    added old code

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Devin Boucher
+// Created on: April 2025
+// This file contains the JS functions for index.html
+
+'use strict'
+
